f9b9bf470ef410fd69788abb9fec560421a77b21
shirleyzhang2 <40873474+shirleyzhang2@users.noreply.github.com>
2019-05-28 23:50:25 -0400

    Update index.py

--- index.py
+++ index.py
@@ -1,6 +1,5 @@
 import os
 import win32com.client
-import openpyxl
 import random
 from openpyxl import *
 
@@ -14,3 +13,16 @@ def get_excel_indices(ws, index_headings_col, index_values_col, index_start_row)
         excel_index[index_heading] = index_value
         current_row = current_row + 1
     return excel_index
+
+wb = load_workbook('SetupAB.xlsx')
+ws = wb.get_sheet_by_name('Index')
+ExcelIndex = Generate_Tower.get_excel_indices(ws, 'A', 'B', 2)
+
+InputTable = ExcelIndex['Input table sheet']
+FloorPlan = ExcelIndex['Floor plans sheet']
+SectionProperties = ExcelIndex['Section properties sheet']
+Bracing = ExcelIndex['Bracing sheet']
+FloorBracing = ExcelIndex['Floor bracing sheet']
+Materials = ExcelIndex['Materials sheet']
+InputTableOffset = ExcelIndex['Input table offset']
+PropertiesStartRow = ExcelIndex['Properties start row']
