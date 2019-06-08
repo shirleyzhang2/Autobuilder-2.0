5b096036645ed0c3d09e6bee06c16695b4999203
AbeKota <41839729+AbeKota@users.noreply.github.com>
2019-06-08 10:50:34 -0400

    Added input table read function
    Function to read input table from Excel and generate a list of tower properties to construct

---
+++ Autobuilder.py
@@ -0,0 +1,8 @@
+import os
+import win32com.client
+import openpyxl
+import random
+from openpyxl import *
+import re
+import time
+
--- ReadExcel.py
+++ ReadExcel.py
@@ -3,6 +3,7 @@ import win32com.client
 import random
 from openpyxl import *
 from openpyxl.utils.cell import get_column_letter, column_index_from_string
+import string
 
 ##########get excel index############
 def get_excel_indices(ws, index_headings_col, index_values_col, index_start_row):
@@ -15,23 +16,6 @@ def get_excel_indices(ws, index_headings_col, index_values_col, index_start_row)
         excel_index[index_heading] = index_value
         current_row = current_row + 1
     return excel_index
-
-wb = load_workbook('SetupAB.xlsx')
-ws_index = wb.get_sheet_by_name('Index')
-ExcelIndex = get_excel_indices(ws_index, 'A', 'B', 2)
-
-InputTable = ExcelIndex['Input table sheet']
-FloorPlan = ExcelIndex['Floor plans sheet']
-SectionProperties = ExcelIndex['Section properties sheet']
-Bracing = ExcelIndex['Bracing sheet']
-FloorBracing = ExcelIndex['Floor bracing sheet']
-Materials = ExcelIndex['Materials sheet']
-InputTableOffset = ExcelIndex['Input table offset']
-PropertiesStartRow = ExcelIndex['Properties start row']
-
-for keys,values in ExcelIndex.items():
-    print(keys)
-    print(values)
 
 ##########read excel tabs############
 def get_properties(ws,headings_start_col, values_start_col, start_row):
@@ -62,14 +46,128 @@ def get_properties(ws,headings_start_col, values_start_col, start_row):
         current_value_col = get_column_letter(column_index_from_string(current_value_col)+3)
     return parameter_type
 
+
+
+class Tower:
+    def __init__(self, number, member_mat, rod_mat, floor_plans, floor_heights, col_props, bracing_types, floor_masses, floor_bracing_types):
+        self.number = number
+        self.member_mat = member_mat
+        self.rod_mat = rod_mat
+        self.floor_plans = floor_plans
+        self.floor_heights = floor_heights
+        self.col_props = col_props
+        self.bracing_types = bracing_types
+        self.floor_masses = floor_masses
+        self.floor_bracing_types = floor_bracing_types
+
+#Outputs a list containing tower objects representing each tower to be built
+def read_input_table(wb,excel_index):
+    #Read in the top value in the sheet
+    input_table_sheet = excel_index['Input table sheet']
+    #Set worksheet to the input table sheet
+    ws_input = wb.get_sheet_by_name('Input Table')
+    input_table_offset = excel_index['Input table offset']
+    total_towers = excel_index['Total number of towers']
+    cur_tower_row = 1
+    table_start_row = cur_tower_row + input_table_offset
+    all_towers = []
+    cur_tower_num = 1
+    while cur_tower_num <= total_towers:
+        #create tower object
+        cur_tower = Tower(0,0,0,0,0,0,0,0,0)
+        #read member material
+        member_mat = ws_input['B'+str(cur_tower_row + 1)].value
+        cur_tower.member_mat = member_mat
+        #read rod material
+        rod_mat = ws_input['B'+str(cur_tower_row + 2)].value
+        cur_tower.rod_mat = rod_mat
+        #read floor plans
+        floor_pln_col = excel_index['Floor plan col']
+        cur_floor_row = cur_tower_row + input_table_offset
+        floor_plans = []
+        while ws_input[floor_pln_col + str(cur_floor_row)].value is not None:
+            floor_pln = ws_input[floor_pln_col + str(cur_floor_row)].value
+            floor_plans.append(floor_pln)
+            cur_floor_row = cur_floor_row + 1
+        cur_tower.floor_plans = floor_plans
+        #read floor heights
+        floor_heights_col = excel_index['Floor height col']
+        cur_floor_row = cur_tower_row + input_table_offset
+        floor_heights = []
+        while ws_input[floor_heights_col + str(cur_floor_row)].value is not None:
+            floor_height = ws_input[floor_heights_col + str(cur_floor_row)].value
+            floor_heights.append(floor_height)
+            cur_floor_row = cur_floor_row + 1
+        cur_tower.floor_heights = floor_heights
+        #read column properties
+        col_props_start_col = excel_index['Column properties start']
+        col_props_end_col = excel_index['Column properties end']
+        cur_floor_row = cur_tower_row + input_table_offset
+        cur_col = col_props_start_col
+        col_props = []
+        while ws_input[cur_col + str(cur_floor_row)].value is not None:
+            print(col_props)
+            while cur_col != chr(ord(col_props_end_col)+1):
+                col_prop = ws_input[cur_col + str(cur_floor_row)].value
+                col_props.append(col_prop)
+                cur_col = chr(ord(cur_col) + 1)
+            cur_col = col_props_start_col
+            cur_floor_row = cur_floor_row + 1
+        cur_tower.col_props = col_props
+        #read bracing properties
+        bracing_types_start_col = excel_index['Bracing type start']
+        bracing_types_end_col= excel_index['Bracing type end']
+        cur_floor_row = cur_tower_row + input_table_offset
+        cur_col = bracing_types_start_col
+        bracing_types = []
+        while ws_input[cur_col + str(cur_floor_row)].value is not None:
+            while cur_col != chr(ord(bracing_types_end_col)+1):
+                bracing_type = ws_input[cur_col + str(cur_floor_row)].value
+                bracing_types.append(bracing_type)
+                cur_col = chr(ord(cur_col) + 1)
+            cur_col = bracing_types_start_col
+            cur_floor_row = cur_floor_row + 1
+        cur_tower.bracing_types = bracing_types
+        #read floor masses
+        floor_masses_col = excel_index['Floor mass col']
+        cur_floor_row = cur_tower_row + input_table_offset
+        floor_masses = []
+        while ws_input[floor_masses_col + str(cur_floor_row)].value is not None:
+            floor_mass = ws_input[floor_masses_col + str(cur_floor_row)].value
+            floor_masses.append(floor_mass)
+            cur_floor_row = cur_floor_row + 1
+        cur_tower.floor_masses = floor_masses
+        #read floor bracing types
+        floor_bracing_col = excel_index['Floor bracing col']
+        cur_floor_row = cur_tower_row + input_table_offset
+        floor_bracing_types = []
+        while ws_input[floor_bracing_col + str(cur_floor_row)].value is not None:
+            floor_bracing = ws_input[floor_bracing_col + str(cur_floor_row)].value
+            floor_bracing_types.append(floor_bracing)
+            cur_floor_row = cur_floor_row + 1
+        cur_tower.floor_bracing_types = floor_bracing_types
+        #increment
+        cur_tower.number = cur_tower_num
+        all_towers.append(cur_tower)
+        cur_tower_num = cur_tower_num + 1
+        cur_tower_row = cur_floor_row + 1
+    return all_towers
+
+
+'''
+#testing
+
+wb = load_workbook('SetupAB.xlsx')
+
+ws_index = wb.get_sheet_by_name('Index')
+ExcelIndex = get_excel_indices(ws_index, 'A', 'B', 2)
+
 ws_section = wb.get_sheet_by_name('Section Properties')
-SectionProperties = get_properties(ws_section, 'A', 'B',4) #or use ExcelIndex 
+SectionProperties = get_properties(ws_section, 'A', 'B',4) #or use ExcelIndex
 
 ws_materials = wb.get_sheet_by_name('Materials')
 Materials = get_properties(ws_materials,'A','B',4)
 
-
-#testing
 for keys,values in SectionProperties.items():
     print(keys)
     print(values)
@@ -79,8 +177,30 @@ for keys,values in Materials.items():
     print(keys)
     print(values)
 
+InputTable = ExcelIndex['Input table sheet']
+FloorPlan = ExcelIndex['Floor plans sheet']
+SectionProperties = ExcelIndex['Section properties sheet']
+Bracing = ExcelIndex['Bracing sheet']
+FloorBracing = ExcelIndex['Floor bracing sheet']
+Materials = ExcelIndex['Materials sheet']
+InputTableOffset = ExcelIndex['Input table offset']
+PropertiesStartRow = ExcelIndex['Properties start row']
+
+for keys,values in ExcelIndex.items():
+    print(keys)
+    print(values)
 
 
-#def read_input_table(ws,floor_col,properties_col,floor_row):
 
-
+AllTowers = read_input_table(wb, ExcelIndex)
+for tower in AllTowers:
+    print(tower.number)
+    print(tower.member_mat)
+    print(tower.rod_mat)
+    print(tower.floor_plans)
+    print(tower.floor_heights)
+    print(tower.col_props)
+    print(tower.bracing_types)
+    print(tower.floor_masses)
+    print(tower.floor_bracing_types)
+'''
