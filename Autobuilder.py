78e297d0e7584977dac2cdec511ba710f4d2f6d5
AbeKota <41839729+AbeKota@users.noreply.github.com>
2019-06-11 20:27:04 -0400

    Added code to initialize SAP2000

--- Autobuilder.py
+++ Autobuilder.py
@@ -5,4 +5,17 @@ import random
 from openpyxl import *
 import re
 import time
+import ReadExcel
+
+print('Initializing SAP2000 model...')
+# create SAP2000 object
+SapObject = win32com.client.Dispatch('SAP2000v15.SapObject')
+# start SAP2000
+SapObject.ApplicationStart()
+# create SapModel Object
+SapModel = SapObject.SapModel
+# initiaize model
+SapModel.InitializeNewModel()
+# create new blank model
+ret = SapModel.File.NewBlank()
 
